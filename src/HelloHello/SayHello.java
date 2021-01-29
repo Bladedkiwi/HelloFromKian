61b89f2cce4e91b7ef9b76b50540784a51fd295d
fairlite2u <pyl95001@byui.edu>
2021-01-29 14:44:56 -0700

    It's just good manners to say hi back

--- src/HelloHello/SayHello.java
+++ src/HelloHello/SayHello.java
@@ -3,5 +3,6 @@ package HelloHello;
 public class SayHello {
     public static void main(String[] args) {
         System.out.format("Hello, my name is %s", "Kian");
+        System.out.format("\nHello, Kian, my name is %s.", "Jennifer");
     }
 }
